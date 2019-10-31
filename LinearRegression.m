05a1fcb41de81a152fddfbdd0992ceae590180a3
NielsNicolai <53909444+NielsNicolai@users.noreply.github.com>
2019-10-31 17:12:13 -0400

    Cleanup code

--- LinearRegression.m
+++ LinearRegression.m
@@ -10,19 +10,17 @@ clear all
 close all
 
 % Define sampled data (u: input; y: output; t: time)
-% u = [zeros(50,1);0.5*ones(50,1)];
-% y = [-0.00400;0.00700;0.0330;0.0150;-0.0160;-0.00900;-0.00400;-0.0100;-0.0230;-0.0330;-0.0150;0.00400;0.0110;0;0.0170;0.0100;0.0140;0.0170;0.0200;-0.00900;-0.0110;0.00800;0.00500;0.00300;-0.00500;-0.00700;0.0180;-0.00300;0.00200;-0.0390;0.00100;-0.0190;-0.0240;0.00300;0.0180;0.0180;0.0150;0.00900;0.0340;0.0360;0.00800;0.0110;0.0130;0.0110;0.00600;0.00600;0.00400;0.0110;0.0160;0.0130;0.00700;0.0660;0.140;0.188;0.209;0.239;0.297;0.334;0.426;0.441;0.444;0.470;0.483;0.507;0.543;0.563;0.554;0.537;0.534;0.514;0.537;0.561;0.551;0.564;0.559;0.576;0.567;0.549;0.553;0.570;0.587;0.575;0.602;0.605;0.594;0.610;0.596;0.557;0.533;0.540;0.549;0.594;0.585;0.571;0.557;0.542;0.535;0.553;0.551;0.531];
-dataorig=csvread('C:\Users\ehaid\Documents\GitHub\GitHub-Tutorial\stepExperiments.csv');
+dataorig=csvread('stepExperiments.csv');
 u=dataorig(:,1);
 y=dataorig(:,2);
-%************FL****************
+
 t = 1:length(u);
 
 % Construct regressor
 phi = [y(1:end-1) u(1:end-1)];
 
 % Least squares regression
-theta = phi\y(2:end); %[0.95;0.5e-1];
+theta = phi\y(2:end);
 
 % Predicted output with estimated parameters
 y_pred(1) = 0;
@@ -32,11 +30,10 @@ for i = t(1:end-1)
 end
 
 % Plot the data with predictions
-subplot(1,2,1);
-
+subplot(2,1,1);
 plot(t,y,'k*',t,y_pred,'b','LineWidth',1,'MarkerSize',1)
-subplot(1,2,2)
-plot(t,u,'k*',t,y_pred,'b','LineWidth',1,'MarkerSize',1)
+subplot(2,1,2)
+stairs(t,u,'b')
 
 
 
